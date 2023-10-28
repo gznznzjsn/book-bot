0a0e0b3e2bc78c359e234ece69ed5b2bdd3033b3
gznznzjsn <arik2046@gmail.com>
2023-10-28 14:39:52 +0300

    Removed unnecessary actions

--- src/main/scala/bookbot/server/CommandsBot.scala
+++ src/main/scala/bookbot/server/CommandsBot.scala
@@ -11,13 +11,6 @@ import zio.interop.catz._
 
 import scala.util.Try
 
-/**
- * Showcases different ways to declare commands (Commands + RegexCommands).
- *
- * Note that non-ASCII commands are not clickable.
- *
- * @param token Bot's token.
- */
 case class CommandsBot(
                         token: String,
                         bookService: BookService
@@ -27,43 +20,18 @@ case class CommandsBot(
     AsyncHttpClientZioBackend.usingClient(
       zio.Runtime.default, asyncHttpClient()
     )
-  )
-    with Polling[Task]
-    with Commands[Task]
-    with RegexCommands[Task] {
+  ) with Polling[Task] with Commands[Task] with RegexCommands[Task] {
 
-  // Extractor
-  object Int {
-    def unapply(s: String): Option[Int] = Try(s.toInt).toOption
-  }
-
-  // Several commands can share the same handler.
-  // Shows the 'using' extension to extract information from messages.
-  onCommand("/hallo" | "/bonjour" | "/ciao" | "/hola") { implicit msg =>
-    using(_.from) { // sender
-      user =>
-        reply(s"Hello ${user.firstName} from Europe?").ignore
-    }
-  }
-
-  def secretIsValid(msg: Message): ZIO[Any, Nothing, Boolean] =
-    ZIO.succeed(msg.text.fold(false)(_.split(" ").last == "password"))
-
-  whenF[Task, Message](onCommand("secret"), secretIsValid) { implicit msg =>
-    reply("42").ignore
-  }
-
-  // withArgs extracts command arguments.
   onRegex("""create\s+([a-zA-Z\s]+) by ([a-zA-Z\s*]+)""".r) {
     implicit msg => {
       case Seq(title, author) =>
         for {
-          _ <- reply("Started creation...")
           book <- bookService.create(title, author)
           _ <- reply(s"Book (${book.title}, ${book.author}) is created with id = ${book.id}")
         } yield ()
     }
   }
+
 }
 
 object CommandsBot {
